e2dde797770f6ffdb6fd4aae83f25836cd7947d9
Andrei Kuniutka <akuniutka@gmail.com>
2024-09-14 16:22:14 +0300

    Feat: Add JPA annotations and DTO

---
+++ src/main/java/ru/practicum/user/NewUserDto.java
@@ -0,0 +1,11 @@
+package ru.practicum.user;
+
+import lombok.Data;
+
+@Data
+public class NewUserDto {
+
+    private String firstName;
+    private String lastname;
+    private String email;
+}
--- src/main/java/ru/practicum/user/User.java
+++ src/main/java/ru/practicum/user/User.java
@@ -1,11 +1,39 @@
 package ru.practicum.user;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
+import java.time.Instant;
+
+@Entity
+@Table(name = "users", schema = "public")
 @Data
+@EqualsAndHashCode(of = "id")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
     private String email;
-    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private UserState state;
+
+    @Column(name = "registration_date")
+    private Instant registrationDate = Instant.now();
 }
